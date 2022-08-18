063129fbdfc9afc0a1da8e3df928f941d70a8c80
jck-bit <jckkinyanjui@gmail.com>
2022-08-18 21:02:26 +0300

    Added flash when user logs in

--- store_manager/auth.py
+++ store_manager/auth.py
@@ -1,10 +1,23 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login', methods=['GET', 'POST'])
+@auth.route('/', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    if request.method == 'POST':
+        
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        if username == 'admin' and password == 'admin':
+            flash('You have been logged in.', category='success')
+            return render_template('home.html')
+
+        else:
+            flash('Invalid credentials.', category='error')
+            return render_template('login.html')
+      
+
 
 @auth.route('/logout')
 def logout():
