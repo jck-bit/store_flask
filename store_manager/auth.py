794e92120c8df31118d7018c1063e0451f1e3606
jck-bit <jckkinyanjui@gmail.com>
2022-08-18 18:05:06 +0300

    logout-route

--- store_manager/auth.py
+++ store_manager/auth.py
@@ -1,14 +1,15 @@
 from flask import Blueprint, render_template
 
-
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template('login.html')
 
-
 @auth.route('/logout')
 def logout():
     return render_template('login.html')
 
+@auth.route('/create_account')
+def create_account():
+    return render_template('signup.html')
--- store_manager/models.py
+++ store_manager/models.py
@@ -1,5 +1,5 @@
 from .import db
-
+from flask_login import UserMixin
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
